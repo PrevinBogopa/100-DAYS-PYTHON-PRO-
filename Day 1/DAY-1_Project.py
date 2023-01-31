6cefcf864cb01e8d40ee9b4b133a2a58b295b7dc
PrevinBogopa <previnbogopa@icloud.com>
2023-01-31 20:04:42 +0200

    all day one

---
+++ Day 1/DAY-1_Project.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Hey i hope you doing good")
+#2. Ask the user for the city that they grew up in.
+city=input("kindly tell me the name of your city? ")
+#3. Ask the user for the name of a pet.
+pet=input("kindly tell me the name of your pet? ")
+#4. Combine the name of their city and pet and show them their band name.
+print(city+pet)
+#5. Make sure the input cursor shows on a new line:
+print("\n")
